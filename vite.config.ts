612250352ee97ed40a4d2f384aba2a6ad5a9393c
Kyle Maxwell <kyle.maxwell@reddit.com>
2025-07-03 20:00:14 -0700

    fix(ci): Set correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: "example",
-  base: "/tilemapper/",
+  base: "/kyle-maxwell/tilemapper/",
   build: {
     outDir: "../docs",
     rollupOptions: {
