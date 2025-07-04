a6a47df7303ea219d0653c999cfa78ce95d3559d
Kyle Maxwell <kyle.maxwell@reddit.com>
2025-07-04 07:53:26 -0700

    fix(deploy): configure vite base path and add .nojekyll

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "example",
   base: "/tilarium/",
   build: {
-    outDir: "../docs",
+    outDir: "docs",
+    emptyOutDir: true,
   },
+  plugins: [react()],
 });
