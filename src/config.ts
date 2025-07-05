d979930dc97310f07b046b47483a156f3b9cba9d
Kyle Maxwell <kyle.maxwell@reddit.com>
2025-07-05 11:31:48 -0700

    WIP: Implement autotiling system

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,21 @@
+type Direction = "N" | "E" | "S" | "W";
+
+// Helper to generate all permutations of direction strings without duplicates
+type Permutations<T extends string, U extends string = T> = [T] extends [never]
+  ? ""
+  : T extends any
+  ? `${T}${"" | Permutations<Exclude<U, T>>}`
+  : never;
+
+type DirectionCombo = Permutations<Direction>;
+
 export interface TileDefinition {
   displayName: string;
+  autotile?: {
+    group: string;
+    neighbors: DirectionCombo;
+  };
+  groupName?: string;
   src: string;
   zIndex: number;
   type: "tile" | "background";
@@ -11,14 +27,15 @@ export interface TileDefinition {
   };
 }
 
-export interface TileGroup {
+export interface TileTabGroup {
   displayName: string;
   tileIds: string[];
+  autotileGroups: string[];
 }
 
 export interface TileConfig {
   tiles: Record<string, TileDefinition>;
-  groups: Record<string, TileGroup>;
+  groups: Record<string, TileTabGroup>;
   mapSize: { width: number; height: number } | "infinite"; // in tiles, not pixels
   defaultZoom: number;
   gridSize: number;
